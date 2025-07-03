c5cfef67771541f7dc303ff20bd0ee7256b31908
ChrisM679 <cmunson@student.neumont.edu>
2025-07-02 22:15:23 -0600

    Ran and Math h

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,11 +1,21 @@
-#include "Core/Math.h"
+#include "Math/Math.h"
+#include "Core/Random.h"
 #include <iostream>
 
 #define NAME "Chris"
 
+using namespace viper;
+
 int main() 
 {
+	//const float deg = viper::math::radToDeg(math::pi);
+	//math::clamp(1, 3, 5);
+
 	std::cout << NAME << std::endl;
 	std::cout << "Hello World!" << std::endl;
-	std::cout << viper::math::pi << std::endl;
+	std::cout << math::pi << std::endl;
+
+	for (int i = 0; i < 10; i++) { 
+		std::cout << random::getRandomFloat() << std::endl;
+	}
 }
